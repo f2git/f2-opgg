916c968d2150ac410de6158312d8efeac9a5216f
f2git <43571670+f2git@users.noreply.github.com>
2022-08-01 12:40:00 +0900

    Header 컴포넌트 생성 및 전역 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
+import Header from '../components/common/Header';
 import GlobalStyle from '../styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </>
   );
