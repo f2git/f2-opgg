95f30368a405bca24329359ecb2b1bd8f3e8e860
f2git <43571670+f2git@users.noreply.github.com>
2022-08-02 16:54:51 +0900

    공용 챔피언 아바타 컴포넌트 제작
    - 공통으로 많이 적용되는 챔피언 아바타는 컴포넌트로 뺌
    - 아바타 클릭시 챔피언 페이지 새창 띄우기
    - 데이터 이상하게 들어오는 경우 작업 완료
      ( 중복된 챔피언 정보, 주소 //, 띄어쓰기 있는 경우) 등

--- components/most/MostChampList.tsx
+++ components/most/MostChampList.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { useAppDispatch, useAppSelector } from '../../store';
 import Colors from '../../styles/Colors';
 import { ChampionType, MostInfoType, MostOptionType, RecentWinRateType } from '../../types/mostInfo';
+import ChampAvatar from '../common/ChampAvatar';
 
 const MostChampListContainer = styled.div`
   background-color: ${Colors.background};
@@ -25,13 +26,16 @@ const MostChampList = ({ mostOption, mostInfo }: IProps) => {
   const { champions, recentWinRate } = mostInfo!;
   const listItem: ChampionType[] | RecentWinRateType[] = mostOption === '챔피언 승률' ? champions : recentWinRate;
 
+  console.log(champions);
+
   return (
     <MostChampListContainer>
       {listItem.map((item, index) => {
-        const key = `${index} ${item.id}`;
+        const key = `${index}-${item.id}-${mostOption}`; // API 데이터에 key, id, name 등이 중복된 경우가 있어 부득이 index 사용
         return (
           <div className="list-item" key={key}>
-            {item.name}
+            <ChampAvatar imageUrl={item.imageUrl} champName={item.key} size="45px" />
+            {/* {item.name} */}
           </div>
         );
       })}
