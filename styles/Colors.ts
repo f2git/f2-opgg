85e5fba6f034d9e8ee975388772c1d37b26b6afc
f2git <43571670+f2git@users.noreply.github.com>
2022-08-03 12:53:04 +0900

    kda, 승률, 평점 색상 추가 및 KDA 색상 함수 작성

--- styles/Colors.ts
+++ styles/Colors.ts
@@ -1,4 +1,4 @@
-export default {
+const Pallet = {
   header: '#1ea1f7',
   mainBlue: '#1f8ecd',
   background: '#eaeaea',
@@ -7,4 +7,24 @@ export default {
   normalGray: '#879292',
   almostBlack: '#555e5e',
   almostBlackPlusFewRed: '#5e5e5e',
+  darkRed: '#c6443e',
+  kda3: '#2daf7f',
+  kda4: '#1f8ecd',
+  kda5: '#e19205',
+  winRate: '#c6443e',
+  point: '#e19205',
 };
+
+export const getKDAColor = (kda: number) => {
+  let color = '';
+  if (kda >= 5) {
+    color = Pallet.kda5;
+  } else if (kda >= 4) {
+    color = Pallet.kda4;
+  } else if (kda >= 3) {
+    color = Pallet.kda3;
+  }
+  return color;
+};
+
+export default Pallet;
