10a1e5a5ece5a6682b151af9cbd9946e420f7214
f2git <43571670+f2git@users.noreply.github.com>
2022-08-06 09:14:50 +0900

    서치바 자동완성, 이전기록 모달 띄우고 닫기
    - 모달 띄우고 닫기
    - 과도하게 빈번한 api 요청을 방지하기 위해 디바운스 적용

--- components/searchBar/SearchBar.tsx
+++ components/searchBar/SearchBar.tsx
@@ -1,42 +1,83 @@
+import React, { useRef, useState } from 'react';
 import Image from 'next/image';
 import styled from 'styled-components';
 import { default as GS } from '../../styles/GeneralStyle';
 import ggLogo from '../../public/images/icon-gg.svg';
+import AutocompleteModal from './AutocompleteModal';
+import RecentKeywordModal from './RecentKeywordModal';
+import useOutsideClick from '../../utils/UseOutsideClick';
+import useDebounce from '../../utils/UseDebounce';
 
-const SearchBarContainer = styled.div`
-  ${GS.FlexRow}
+const SearchBarContainer = styled.div<{ modal: boolean }>`
   width: 260px;
   height: 32px;
-  background-color: white;
   margin-bottom: 12px;
-  border-radius: 2px;
-  flex-direction: row;
-  .input-area {
-    ${GS.FlexRowVerticalCenter}
-    flex:1;
-    input {
-      width: 100%;
-      height: 100%;
-      border: none;
-      outline: none;
-      padding-left: 14px;
-      padding-top: 5px;
-      padding-bottom: 5px;
+  .search-bar {
+    ${GS.FlexRow}
+    background-color: white;
+    border-radius: 2px;
+    flex-direction: row;
+    .input-area {
+      ${GS.FlexRowVerticalCenter}
+      flex:1;
+      input {
+        width: 100%;
+        height: 100%;
+        border: none;
+        outline: none;
+        margin-left: 14px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+      }
+    }
+    .button-area {
+      width: 40px;
+      cursor: pointer;
     }
   }
-  .button-area {
-    width: 40px;
+  .bottom-modal-area {
+    position: relative;
+    margin-top: 4px;
+    width: 100%;
+    background-color: white;
+    box-shadow: rgb(0 0 0 / 50%) 0px 2px 4px 0px;
+    visibility: ${({ modal }) => (modal ? 'visible' : 'hidden')};
+    z-index: 1;
   }
 `;
 
 const SearchBar = () => {
+  const [debouncedKeyword, keyword, setKeyword] = useDebounce<string>('', 200);
+  const [bottomModal, setBottomModal] = useState(false);
+  const componentREf = useRef<HTMLDivElement>(null);
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setKeyword(e.currentTarget.value);
+  };
+
+  const modalToggle = (focused: boolean) => {
+    setBottomModal(focused);
+  };
+
+  useOutsideClick(componentREf, () => modalToggle(false));
+
   return (
-    <SearchBarContainer>
-      <div className="input-area">
-        <input placeholder="소환사명,챔피언···" />
+    <SearchBarContainer ref={componentREf} modal={bottomModal}>
+      <div className="search-bar">
+        <div className="input-area">
+          <input
+            placeholder="소환사명,챔피언···"
+            value={keyword}
+            onChange={onChange}
+            onFocus={() => modalToggle(true)}
+          />
+        </div>
+        <div className="button-area">
+          <Image src={ggLogo} width={32} height={32} priority />
+        </div>
       </div>
-      <div className="button-area">
-        <Image src={ggLogo} width={32} height={32} />
+      <div className="bottom-modal-area">
+        {keyword ? <AutocompleteModal keyword={debouncedKeyword} /> : <RecentKeywordModal />}
       </div>
     </SearchBarContainer>
   );
