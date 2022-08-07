9bd13b757f00aca0865d72d72f13684cc83abe84
f2git <43571670+f2git@users.noreply.github.com>
2022-08-07 16:21:49 +0900

    타이틀 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>롤 전적 검색 OP.GG</title>
-        <meta name="description" content="OP.GG 사전과제 입니다" />
-      </Head>
-      <div>/</div>
-    </>
-  );
+  return <div>/</div>;
 };
 
 export default Home;
